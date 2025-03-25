0c845674b2ba3b7c02ff4f06202be43f2fff4e54
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2025-03-25 13:55:12 +0900

    update test

--- tests/python/non-mpi/bsetool_SCL/test_bsetool_SCL.py
+++ tests/python/non-mpi/bsetool_SCL/test_bsetool_SCL.py
@@ -19,9 +19,6 @@ def test_chiq(request):
     assert os.system("bse_post.py bse.toml") == 0
 
     # compare results (take rounding errors into account)
-    chi0q = read_chiq('chi0_q_eigen.dat')
-    chi0q_ref = read_chiq('ref/chi0_q_eigen.dat')
-    assert np.allclose(chi0q, chi0q_ref)
 
     chiq = read_chiq('chi_q_scl_eigen.dat')
     chiq_ref = read_chiq('ref/chi_q_scl_eigen.dat')
