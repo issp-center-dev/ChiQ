6d5ac1290e014fdf6621174f06b863ac26ce51de
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2025-06-23 15:19:05 +0900

    change plot color in plot_chiqw.py

--- python/scripts/plot_chiqw.py
+++ python/scripts/plot_chiqw.py
@@ -10,18 +10,19 @@ import matplotlib
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 
 def main():
     P = argparse.ArgumentParser()
     P.add_argument("file_qpath", help="path of q-path file")
     P.add_argument("dir_chiqw", nargs="?", default=".", help="directory of input files")
-    P.add_argument("--input_file_prefix", default="chiq_w", help="prefix of input file (e.g., 'chiq_w' for 'chiq_w__XX.YY.ZZ.dat')")
+    P.add_argument("--input_file_prefix", default="chi_q_w", help="prefix of input file (e.g., 'chi_q_w' for 'chi_q_w__XX.YY.ZZ.dat')")
     P.add_argument("--ielem", type=int, default=0, help="index of element to plot")
     P.add_argument("--wmin", type=float, default=None, help="minimum frequency to plot")
     P.add_argument("--wmax", type=float, default=None, help="maximum frequency to plot")
     P.add_argument("--part", type=str, default="real", choices=["real", "imag"], help="part of susceptibility to plot")
-    P.add_argument("--output_file_prefix", default="chiq_w", help="base name of output file (e.g., 'chiq_w' for 'chiq_w.pdf')")
+    P.add_argument("--output_file_prefix", default="chi_q_w", help="base name of output file (e.g., 'chi_q_w' for 'chi_q_w.pdf')")
     P.add_argument(
         "--format",
         default="pdf",
@@ -82,7 +83,7 @@ def main():
                     f.write(f"{xarray[iq]} {ws[iw]} {chiqw_re[iq, iw]} {chiqw_im[iq, iw]}\n")
         print(f"Data written to {args.data_out}")
 
-    im = ax.pcolormesh(X, Y, to_be_plotted.T, shading='auto', cmap='RdBu_r')
+    im = ax.pcolormesh(X, Y, to_be_plotted.T, shading='auto', cmap='RdBu_r', norm=colors.CenteredNorm())
     fig.colorbar(im, ax=ax, label=label_chiqw)
 
     # Set x-axis ticks and labels
